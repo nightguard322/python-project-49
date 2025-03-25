35134b300db1031b9560bfbb3cb6e60e28dd07b1
sasha <sander_@inbox.ru>
2025-03-25 15:08:29 +0300

    added gcd

---
+++ brain_games/games/gcd.py
@@ -0,0 +1,37 @@
+from brain_games import utils
+from brain_games.games.engine import launch
+import random
+
+def start() -> None:
+
+    expressions = generate_expressions()
+    config = {
+        'message': "Find the greatest common divisor of given numbers.",
+        'expressions': expressions
+    }
+    launch(config)
+
+def generate_expressions() -> list:
+    expressions = []
+    numbers = generate_number_pairs()
+
+    for num1, num2 in numbers:
+        expressions.append(
+            (f"{num1} {num2}", calculate(num1, num2))
+        )
+        
+    return expressions
+
+def calculate(num1: int, num2: int) -> int:
+    if num2 == 0:
+        return 0
+        
+    while num1 % num2 != 0:
+        num1, num2 = num2, num1 % num2
+    return num2
+
+def generate_number_pairs() -> list:
+    first_numbers_list = [random.randint(1, 100) for _ in range(3)]
+    second_numbers_list = [random.randint(1, 100) for _ in range(3)]
+    return zip(first_numbers_list, second_numbers_list)
+    
---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,9 @@
+from brain_games import utils
+from brain_games.games import gcd
+
+def main():
+    utils.greet()
+    gcd.start()
+
+if __name__ == "__main__":
+    main()
